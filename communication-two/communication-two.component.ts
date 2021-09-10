73d77435de26026d30b2a486b6669497601cb3e9
Deepak Bawa <bawa_d@ymail.com>
2021-09-10 11:13:44 +0530

    Create communication-two.component.ts

---
+++ communication-two/communication-two.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { DataToBeSaved } from "../interfaces/data-to-be-saved";
+
+@Component({
+  selector: 'app-communication-two',
+  templateUrl: './communication-two.component.html',
+  styleUrls: ['./communication-two.component.css']
+})
+export class CommunicationTwoComponent implements OnInit {
+
+  data_to_be_saved!: DataToBeSaved;
+
+  @Input() first_name!: string;
+  @Input() last_name!: string;
+  @Input() email!: string;
+
+  @Output() changeHost = new EventEmitter();
+  
+  constructor() {
+    console.log('constructor 2', this.data_to_be_saved);
+  }
+
+  ngOnInit(): void {
+    this.data_to_be_saved = {
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email
+    };
+    console.log('constructor 3', this.data_to_be_saved);
+  }
+
+  handleClick($event: any) {
+    this.data_to_be_saved = {
+      first_name: this.first_name +'1',
+      last_name: this.last_name +'1',
+      email: this.email +'1'
+    };
+
+    console.log('CommunicationTwoComponent handleClick', this.data_to_be_saved);
+    this.changeHost.emit(this.data_to_be_saved);
+  }
+}
